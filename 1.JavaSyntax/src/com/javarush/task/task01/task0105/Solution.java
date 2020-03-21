f441e17e7a9d8fe67a71244c7e317186e84045b5
mikhailshepelev <romanist-mike@yandex.ru>
2020-03-21 14:52:55 +0200

    Commit 0 level Syntax Quest

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0105/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task01.task0105;
+
+/* 
+Объявляем переменные
+*/
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+    String name = "name";
+    int age = 30;
+    String city = "city";
+    
+        //напишите тут ваш код
+    }
+}
